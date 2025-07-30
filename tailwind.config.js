f5279e21e954ae69da68673ef258f13fbcbc9acb
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-07-30 14:06:25 -0300

    ahora si

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,24 @@
-/home/gonik/Documents/git/goodvibes-org/api_gen2/gv-app/tailwind.config.js
+    //    // tailwind.config.js
+    // module.exports = {
+    //   content: [
+    //     './src/**/*.rs', // Adjust path to your Leptos components
+    //     './index.html', // If you have a root HTML file
+    //   ],
+    //   theme: {
+    //     extend: {},
+    //   },
+    //   plugins: [],
+    // };
+    //
+    //
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.rs",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
